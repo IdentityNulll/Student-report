51c5caf1845f0100315eaf1128da3e97d89ceb5d
Identitynull <identitynulll@gmail.com>
2026-03-06 10:16:24 +0500

    login fully completed

--- src/layouts/StudentLayout.jsx
+++ src/layouts/StudentLayout.jsx
@@ -1,4 +1,6 @@
-export default function StudentLayout() {
+import { Outlet } from "react-router-dom";
+
+export default function TeacherLayout() {
   return (
     <div className="flex">
       <aside className="w-64 bg-gray-900 text-white h-screen p-4">
