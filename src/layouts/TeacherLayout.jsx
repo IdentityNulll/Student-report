b27489a3168814609abafe6d3e9e752acd58050d
Identitynull <identitynulll@gmail.com>
2026-03-21 02:48:31 +0500

    3am commit

--- src/layouts/TeacherLayout.jsx
+++ src/layouts/TeacherLayout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 export default function TeacherLayout() {
   return (
-    <div className="flex">
-      <aside className="w-64 bg-gray-900 text-white h-screen p-4">
-        Teacher Panel
-      </aside>
+    <div className="min-h-screen bg-[var(--bg-main)]">
+      <Sidebar />
 
-      <main className="flex-1 p-6">
+      <main className="ml-20 md:ml-72 p-4 sm:p-6">
         <Outlet />
       </main>
     </div>
